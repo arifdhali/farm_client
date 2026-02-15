95c1b45417f8c5fac91950481badfff143d7d04c
Arif Dhali <arifdhali98@gmail.com>
2026-02-16 01:05:11 +0530

    redirct issue

--- app/components/layout/AdminLayout.tsx
+++ app/components/layout/AdminLayout.tsx
@@ -1,59 +1,43 @@
 import React from "react"
 import { Outlet, redirect, useNavigation } from "react-router"
 import Header from "./Header"
-import Footer from "./Footer"
 import MenuSidebar from "./MenuSidebar"
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar"
+import { SidebarInset, SidebarProvider } from "../ui/sidebar"
 import Loading from "../ui/Loading"
 import queryClient from "@/query/client"
 import { getMeQuery } from "@/query/Auth.queries"
-import type { AxiosError } from "axios"
-import type { BackendError } from "@/types/AxiosErrors.type"
-
 
 export async function loader({ request }: { request: Request }) {
-    const cookie = request.headers.get("cookie") ?? undefined;
-    try {
-        let user = await queryClient.ensureQueryData(getMeQuery(cookie));
-        console.log("Admin layout", user);
-        if (!user) {
-            throw redirect("/auth/login");
-        }
-        return null;
-    } catch (err) {
-        const error = err as AxiosError<BackendError>;
-        if (!error.response?.data.status) {
-            throw redirect("/auth/login");
-        }
-    }
+  const cookie = request.headers.get("cookie") ?? undefined;
+
+  try {
+    const user = await queryClient.ensureQueryData(getMeQuery(cookie));
+
+    if (!user?.id) throw redirect("/auth/login");
+
+    return null;
+  } catch {
+    throw redirect("/auth/login");
+  }
 }
 
 const AdminLayout = () => {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-    return (
-        <>
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <SidebarProvider>
+      <MenuSidebar />
+      <SidebarInset>
+        <Header />
+        <div className="p-6">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
 
-            {
-                isLoading ? (
-                    <Loading />
-                ) : (
-                    <SidebarProvider >
-                        <MenuSidebar />
-
-                        <SidebarInset>
-                            <Header />
-                            <div className="p-6"> <Outlet /></div>
-                        </SidebarInset>
-
-                    </SidebarProvider>
-
-                )
-            }
-
-
-        </>
-    )
-}
-export default AdminLayout
+export default AdminLayout;
--- app/components/layout/AuthLayout.tsx
+++ app/components/layout/AuthLayout.tsx
@@ -1,39 +1,25 @@
 import { getMeQuery } from '@/query/Auth.queries';
 import queryClient from '@/query/client';
-import type { BackendError } from '@/types/AxiosErrors.type';
-import type { AxiosError } from 'axios';
-import React from 'react'
-import { Outlet, redirect } from 'react-router'
-
+import { Outlet, redirect } from 'react-router';
 
 export async function loader({ request }: { request: Request }) {
-    const cookie = request.headers.get("cookie") ?? undefined;
-    try {
-        let user = await queryClient.ensureQueryData(getMeQuery(cookie));
-        console.log("Auth layout", user);
-        if (user?.id) {
-            return redirect("/");
-        }
-        return null;
-    } catch (err) {
-        const error = err as AxiosError<BackendError>;
-        console.log("Auth Error", error);
-        if (error.status === 401) {
-            return null;
-            throw redirect("/auth/login");
-        }
-    }
+  const cookie = request.headers.get("cookie") ?? undefined;
+
+  try {
+    const user = await queryClient.ensureQueryData(getMeQuery(cookie));
+    if (user?.id) return redirect("/");
+    return null;
+  } catch {
+    return null;
+  }
 }
 
 const AuthLayout = () => {
-    return (
+  return (
+    <div className="auth-pages flex justify-center items-center h-screen">
+      <Outlet />
+    </div>
+  );
+};
 
-        <>
-            <div className="auth-pages flex justify-center items-center h-screen">
-                <Outlet />
-            </div>
-        </>
-    )
-}
-
-export default AuthLayout
+export default AuthLayout;
