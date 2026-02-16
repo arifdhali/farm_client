1629a313f7fd5b00b02672a8817b1e09380fd699
Arif Dhali <arifdhali98@gmail.com>
2026-02-16 22:43:28 +0530

    http changes

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const isServer = typeof window === "undefined";
+console.log(isServer)
 const HTTP = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
+  baseURL: isServer
+    ? process.env.API_URL
+    : import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default HTTP;
