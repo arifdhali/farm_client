3bba4b6dae6a4f3cf6b813c6ad1dfb8a040f7cce
Arif Dhali <arifdhali98@gmail.com>
2026-04-20 22:35:41 +0530

    fix medicine price format

--- app/routes/farms/SingleLifting.tsx
+++ app/routes/farms/SingleLifting.tsx
@@ -192,7 +192,7 @@ const SingleLifting = () => {
                                     </div>
                                     <div className="flex items-center gap-1">
                                         <span className="text-[10px] font-medium text-green-600 dark:text-green-400"><IndianRupee /></span>
-                                        <span className="text-2xl font-black text-green-700 dark:text-green-400">{view?.total?.total_medicine_price}</span>
+                                        <span className="text-2xl font-black text-green-700 dark:text-green-400">{view?.total?.total_medicine_price?.toLocaleString("en-IN", { minimumFractionDigits: 2 }) || 0}</span>
                                     </div>
                                 </div>
                                 <div className="bg-red-50 dark:bg-red-950/20 border border-red-100 dark:border-red-900/30 p-5 rounded-xl">
